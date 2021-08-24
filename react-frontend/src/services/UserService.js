b914dc0ae6d45b4192372ecc0f6f5896ac9d128f
swathi-zelarsoft680 <82649007+swathi-zelarsoft680@users.noreply.github.com>
2021-08-24 10:27:54 +0530

    Update UserService.js

--- react-frontend/src/services/UserService.js
+++ react-frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://localhost:8080/api/v1/users";
+const USER_API_BASE_URL = "http://3.85.20.56:8080/api/v1/users";
 
 class UserService {
 
